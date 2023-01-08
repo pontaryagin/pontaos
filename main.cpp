a64661c7f8fd81c5429fcb2736c3427702758d58
pontaryagin <ponta1496@gmail.com>
2023-01-08 16:37:58 +0900

    kernel に引数を渡す

--- main.cpp
+++ main.cpp
@@ -2,7 +2,13 @@
 using uint64_t = unsigned long long;
 using uint8_t = unsigned char;
 
-extern "C" void KernelMain()
+extern "C" void KernelMain(uint64_t frame_buffer_base,
+                           uint64_t frame_buffer_size)
 {
+  auto frame_buffer = reinterpret_cast<uint8_t*>(frame_buffer_base);
+  for(uint64_t i =0; i < frame_buffer_size; ++i)
+  {
+    frame_buffer[i] = i%256;
+  }
   while (1) __asm__("hlt");
 }
